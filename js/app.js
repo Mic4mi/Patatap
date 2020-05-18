f3fd4cb6d3d728807d518785d045269abcc3ffc1
Mic4mi <61918959+Mic4mi@users.noreply.github.com>
2020-05-18 12:47:44 -0300

    Checking if the sounds work

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
+var keyData = {
+    a: {
+        color: "purple",
+        sound: new Howl({
+            src: ['sounds/sounds/bubbles.mp3']
+        })
+    },
+    b: {
+        color: "green",
+        sound: new Howl({
+            src: ['sounds/sounds/clay.mp3']
+        })
+    },
+    c: {
+        color: "yellow",
+        sound: new Howl({
+            src: ['sounds/sounds/confetti.mp3']
+        })
+    },
+};
+
 var circles = [];
 
 function onKeyDown(event) {
-    // Establece los límites de dónde puede aparecer el circulo nuevo
-    var maxPoint = new Point(view.size.width, view.size.height);
-    // obetener un punto aleatorio ya viene incorporado en paperJS
-    var randomPoint = Point.random();
-    // Es un paralelo de math random * math floor
-    var point = maxPoint * randomPoint;
-    var newCircle = new Path.Circle(point, 300);
-    newCircle.fillColor = "orange";
-    circles.push(newCircle);
+    if (keyData[event.key]) {
+        // Establece los límites de dónde puede aparecer el circulo nuevo
+        var maxPoint = new Point(view.size.width, view.size.height);
+        // obetener un punto aleatorio ya viene incorporado en paperJS
+        var randomPoint = Point.random();
+        // Es un paralelo de math random * math floor
+        var point = maxPoint * randomPoint;
+        var newCircle = new Path.Circle(point, 300);
+        newCircle.fillColor = keyData[event.key].color;
+        keyData[event.key].sound.play();
+        circles.push(newCircle);
+    }
 }
 
 function onFrame() {
